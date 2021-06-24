1e66444b6e5986e9643d9065feb9eafb983825a8
ladral <tuor_philipp@hotmail.com>
2021-06-24 20:02:19 +0200

    get todo items from local storage

---
+++ blazor-wasm-todo/Shared/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace blazor_wasm_todo.Shared
+{
+    public class Todo
+    {
+        public int id { get; }
+        public string title { get; }   
+        public Todo(int id, string title)
+        {
+            this.id = id;
+            this.title = title;
+        }
+    }
+}
